d20148f4acb299aa61a55b9b163cf3dd0c88cb4b
muhammad-tamim <contact2tamim@gmail.com>
2025-08-12 04:54:02 +0600

    feat: add about section

---
+++ src/components/Elements/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = ({ title }) => {
+    return (
+        <h1 className="text-5xl font-bold mb-12 text-center text-secondary">
+            {title}
+        </h1>
+    )
+}
+
+export default Heading
--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+import Heading from "../Elements/Heading";
+import { Fade } from "react-awesome-reveal";
 
 const About = () => {
     return (
-        <div>
+        <Fade>
+            <section id="about" className="max-w-5xl mx-auto px-6 py-16">
+                <Heading title="About"></Heading>
 
-        </div>
+                <div className="space-y-8 rounded-2xl shadow-lg p-4 md:p-16  leading-relaxed md:text-lg">
+                    <p className="">
+                        I'm a passionate MERN stack web developer.
+                        My programming journey began a few years ago when I first discovered the power of building websites and applications.
+                        Since then, I have dedicated myself to mastering the full stack, from designing intuitive user interfaces to developing robust backend services.
+                    </p>
+
+                    <p className="">
+                        I especially enjoy crafting clean, efficient code and solving real-world problems through technology.
+                        Whether it’s building scalable web apps, experimenting with new frameworks, or optimizing performance, I find great satisfaction in continuous learning and improvement.
+                    </p>
+
+                    <p className="">
+                        Outside of programming, I’m an avid sports enthusiast and love playing football to stay active and clear my mind.
+                        I also have a creative side—I enjoy painting and exploring digital art, which helps me think outside the box and bring fresh ideas to my projects.
+                    </p>
+
+                    <p className="">
+                        Overall, I believe a balanced blend of technical skills and personal passions shapes me into a developer who not only writes code but tells stories and builds experiences that matter.
+                    </p>
+                </div>
+            </section>
+        </Fade>
     );
 };
 
